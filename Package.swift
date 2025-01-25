0baaccda0b23bcd88819f4a2079b324e3578b5ef
kamimi <awakaru0122@gmail.com>
2025-01-25 16:15:49 -0500

    :pencil: first init

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "PorfolioWithSwiftDocC",
             targets: ["PorfolioWithSwiftDocC"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
